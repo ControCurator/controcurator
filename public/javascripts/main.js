970a2ff92cd9cce9a4c1f6ee4329019fefbc86e3
Benjamin Timmermans <b.timmermans@vu.nl>
2016-10-14 17:33:22 +0200

    Added annotation template. Needs to be connected to python backend.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,40 +1,7 @@
-$(document)
-    .ready(function() {
-      $('.ui.form')
-        .form({
-          fields: {
-            email: {
-              identifier  : 'email',
-              rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your e-mail'
-                },
-                {
-                  type   : 'email',
-                  prompt : 'Please enter a valid e-mail'
-                }
-              ]
-            },
-            password: {
-              identifier  : 'password',
-              rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your password'
-                },
-                {
-                  type   : 'length[6]',
-                  prompt : 'Your password must be at least 6 characters'
-                }
-              ]
-            }
-          }
-        })
-      ;
-    });
-
-    $('.haspopup')
-      .popup()
-    ;
-  ;
+$(document).ready(function() {
+  $('#annotate').click(function() {
+     //$.get('/some/url', {foo: 42}, function(result) {
+         $('.annotation .task').html('Is Immunization a controversial topic?');
+     //});
+  });
+});
