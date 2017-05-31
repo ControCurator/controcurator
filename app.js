32e4dcfbcbb79eecdf00d8efde23a0e79e9247cb
Jorann <j.de.waaij@vu.nl>
2017-05-31 14:13:30 +0200

    More room for data

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
