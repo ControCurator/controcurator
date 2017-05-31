f291423f2ac7221e3b3163d1d3c531ec1814c2be
Jorann <j.de.waaij@vu.nl>
2017-05-31 12:48:15 +0200

    Test voor post

--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,30 @@ router.get('/', function(req, res, next) {
     res.json({'info' : 'This is the ControCurator API v0.1'});
 });
 
+router.post('/',function(req,res) {
+    try {
+        var input = JSON.parse(req);
+    } catch (e) {
+        res.json({'error':'Input was not valid JSON.'});
+    }
+
+    var amountofitems = input.length;
+    var gooditems = 0;
+    var baditems = 0;
+
+    foreach (var currentitem in input)
+    {
+        if (!currentitem.hasOwnProperty('id') || !currentitem.hasOwnProperty('text'))
+        {
+            baditems++;
+            continue;
+        }
+        gooditems++;
+    }
+    var controscore = Math.random();
+
+
+    res.json({'controversy':controscore,'totalItems':amountofitems,'goodItems':gooditems,'badItems':baditems});
+
+});
 module.exports = router;
