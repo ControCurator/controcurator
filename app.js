80bc55b7d289579e030ca3279c8e0c141e9517fb
Jorann <j.de.waaij@vu.nl>
2017-05-31 14:24:21 +0200

    Typo

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.json({limit: '20mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
 app.use(cookieParser());
